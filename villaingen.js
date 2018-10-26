c704873e246009f2d2eb8cf2bcb45cbf108ecd65
fatalst <31542223+fatalst@users.noreply.github.com>
2018-10-25 22:39:04 -0500

    adding html

--- villaingen.js
+++ villaingen.js
@@ -1,9 +1,15 @@
-// pullin in json file
-var jsonData = require('./words.json');
+
+$(document).ready(function() {
+  $( "#btn" ).click(function() {
+    $.getJSON('words.json', function(data) {
+      document.getElementById("id01").innerHTML = (randomCharacteristic(data) + ' ' + randomPerson(data) + ' who ' + randomBackstory(data) + ' and is now ' + randomCurrent(data) + ' in order to ' + randomGoal(data) + '.');
+    });
+  });
+});
 
 // randomly picks a characteristic from the json file and appends A or An to the front, depending on the first letter
-function randomCharacteristic(){
-  var word = jsonData.characteristic[Math.floor(Math.random() * jsonData.characteristic.length)];
+function randomCharacteristic(data){
+  var word = data.characteristic[Math.floor(Math.random() * data.characteristic.length)];
   var firstLetter = word.split("")[0].toLowerCase();
   if (firstLetter == "a" | firstLetter == "e" | firstLetter == "i" | firstLetter == "o" | firstLetter == "u"){
     return 'An ' + word;
@@ -13,38 +19,21 @@ function randomCharacteristic(){
 }
 
 // randomly picks a person from the json file
-function randomPerson(){
-  return jsonData.people[Math.floor(Math.random() * jsonData.people.length)];
+function randomPerson(data){
+  return data.people[Math.floor(Math.random() * data.people.length)];
 }
 
 // randomly picks a backstory from the json file
-function randomBackstory(){
-  return jsonData.backstories[Math.floor(Math.random() * jsonData.backstories.length)];
+function randomBackstory(data){
+  return data.backstories[Math.floor(Math.random() * data.backstories.length)];
 }
 
 // randomly picks a current story from the json file
-function randomCurrent(){
-  return jsonData.currentStories[Math.floor(Math.random() * jsonData.currentStories.length)];
+function randomCurrent(data){
+  return data.currentStories[Math.floor(Math.random() * data.currentStories.length)];
 }
 
 // randomly picks a goal from the json file
-function randomGoal(){
-  return jsonData.goals[Math.floor(Math.random() * jsonData.goals.length)];
+function randomGoal(data){
+  return data.goals[Math.floor(Math.random() * data.goals.length)];
 }
-
-// combines all the randomization into one sentence, returns the full line
-function randomizer(){
-  // currently split into a variable for testing reasons
-  var line = (randomCharacteristic() + ' ' + randomPerson() + ' who ' + randomBackstory() + ' and is now ' + randomCurrent() + ' in order to ' + randomGoal() + '.');
-  return line;
-}
-
-// prints to the console
-console.log("\n\n\n\n\n\n\n\nYOUR VILLAIN IS:\n" + randomizer() + "\n\n\n\n\n\n\n\n");
-
-// console.log(randomizer());
-// console.log(randomizer());
-// console.log(randomizer());
-// console.log(randomizer());
-// console.log(randomizer());
-// console.log(randomizer());
