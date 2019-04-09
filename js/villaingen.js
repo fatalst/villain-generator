e4a05ae1a715cd40c1697159dca14e45320b0698
fatalst <ermur97@gmail.com>
2019-04-09 14:46:55 -0500

    trying to catch that pesky undefined bug

--- js/villaingen.js
+++ js/villaingen.js
@@ -30,23 +30,25 @@ function randomizer(json){
     str1 = randomCharacteristic(data);
   });
   $.getJSON(json, function(data) {
-    str2 = (' ' + data.people[chance.integer({min: 0, max: data.people.length-1})] +
-        ' who has ' + data.backstories[chance.integer({min: 0, max: data.backstories.length-1})] +
-        ' and is now ' + data.currentStories[chance.integer({min: 0, max: data.currentStories.length-1})] +
-        ' in order to ' + data.goals[chance.integer({min: 0, max: data.goals.length-1})] + '.');
+    str2 = (' ' + data.people[chance.integer({min: 0, max: (data.people.length-1)})] +
+        ' who has ' + data.backstories[chance.integer({min: 0, max: (data.backstories.length-1)})] +
+        ' and is now ' + data.currentStories[chance.integer({min: 0, max: (data.currentStories.length-1)})] +
+        ' in order to ' + data.goals[chance.integer({min: 0, max: (data.goals.length-1)})] + '.');
     document.getElementById("vil").innerHTML = str1 + str2;
   });
 }
 
 function newButton(){
   $.getJSON('json/words.json', function(data) {
-    document.getElementById("btn").innerHTML = ('Not ' + data.adj[chance.integer({min: 0, max: data.adj.length-1})] + ' enough!');
+    document.getElementById("btn").innerHTML = ('Not ' + data.adj[chance.integer({min: 0, max: (data.adj.length-1)})] + ' enough!');
   });
 }
 
 // randomly picks a characteristic from the json file and appends A or An to the front, depending on the first letter
 function randomCharacteristic(data){
-  var word = data.characteristic[chance.integer({min: 0, max: (data.characteristic.length-1)})];
+  var len = data.characteristic.length - 1;
+  var word = data.characteristic[chance.integer({min: 0, max: len})];
+  if (typeof word === 'undefined'){ word = data.characteristic[0]; }
   var firstLetter = word.split("")[0].toLowerCase();
   if (firstLetter == "a" | firstLetter == "e" | firstLetter == "i" | firstLetter == "o" | firstLetter == "u"){
     return 'An ' + word;
